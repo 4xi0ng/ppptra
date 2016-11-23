134c88d0cccab5eb002fa38edb88b067a5e80d61
4xi0ng <webxxy@qq.com>
2016-11-24 00:21:38 +0800

    entry

--- readelf.c
+++ readelf.c
@@ -25,7 +25,7 @@ int print_ehdr(char * filename)
 	printf("<--ELF HEADER-->\n");
 
 	//Magic
-	printf("Magic:");
+	printf("    Magic: ");
 	for (int i = 0; i < 16; ++i)
 	{
 		printf(" %02x", ehdr->e_ident[i]);
@@ -33,7 +33,7 @@ int print_ehdr(char * filename)
 	printf("\n");
 	
 	//Class
-	printf("Class: ");
+	printf("    Class: ");
 	switch(ehdr->e_ident[4]){
 		case ELFCLASSNONE: printf("NONE\n");break;
 		case ELFCLASS32: printf("32\n");break;
@@ -41,6 +41,67 @@ int print_ehdr(char * filename)
 		case ELFCLASSNUM: printf("NUM\n");break;
 		default: printf("\n");break;
 	}
+
+	//Data
+	printf("    Data: ");
+	switch(ehdr->e_ident[5]){
+		case ELFDATANONE: printf("NONE\n");break;
+		case ELFDATA2LSB: printf("little endian\n");break;
+		case ELFDATA2MSB: printf("big endian\n");break;
+		case ELFDATANUM: printf("NUM\n");break;
+		default: printf("\n");break;
+	}
+
+	//Version
+	printf("    Version: ");
+	switch(ehdr->e_ident[6]){
+		case EV_NONE: printf("NONE\n");break;
+		case EV_CURRENT: printf("1 current version\n");break;
+		case EV_NUM: printf("NUM\n");break;
+		default: printf("\n");break;
+	}
+
+	//Type
+	printf("    Type: ");
+	switch(ehdr->e_type){
+		case ET_NONE: printf("NONE\n");break;
+		case ET_REL: printf("Relocatable file\n");break;
+		case ET_EXEC: printf("Executable file\n");break;
+		case ET_DYN: printf("Shared object file\n");break;
+		case ET_CORE: printf("Core file\n");break;
+		case ET_NUM: printf("Number of defined types\n");break;
+		case ET_LOOS: printf("OS-specific range start\n");break;
+		case ET_HIOS: printf("OS-specific raneg end\n");break;
+		case ET_LOPROC: printf("Processor-specific range start\n");break;
+		case ET_HIPROC: printf("Processor-specific range end\n");break;
+		default: printf("\n");break;
+	}
+
+	//Machine
+	printf("    Machine: ");
+	switch(ehdr->e_machine){
+		case EM_NONE: printf("NONE\n");break;
+		case EM_M32: printf("AT&T WE 32100\n");break;
+		case EM_386: printf("Intel 80386 \n");break;
+		case EM_860: printf("Intel 80860\n");break;
+		case EM_MIPS: printf("MIPS R3000 big-endian\n");break;
+		case EM_MIPS_RS3_LE: printf("MIPS R3000 little-endian\n");break;
+		case EM_ARM: printf("ARM\n");break;
+		default: printf("\n");break;
+	}
+
+	//Version
+	printf("    Version: ");
+	switch(ehdr->e_version){
+		case EV_NONE: printf("NONE\n");break;
+		case EV_CURRENT: printf("1 current version\n");break;
+		case EV_NUM: printf("NUM\n");break;
+		default: printf("\n");break;
+	}
+
+	//Entry point address
+	/*
+	*/
 
 	return 0;
 }
